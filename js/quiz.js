f1044d316b65f4cf0e24dcedb519bcd7108ce2ea
Clay Giffin <clay@claygiffin.com>
2020-04-16 12:50:29 -0400

    added traveler quiz, styling

--- js/quiz.js
+++ js/quiz.js
@@ -4,15 +4,22 @@ $(document).ready(function () {
   var quizReset = function () {
     quiz.find('.question-wrap').hide()
     quiz.find('#results').hide()
+    quiz.find('#results .result').hide()
+    quiz.find('.illegal, .unknown').removeClass('illegal unknown')
     quiz.find('#q-01').show()
-    quiz.find('.illegal').removeClass('illegal')
+    quiz.find('#reset').html('')
     localStorage.clear()
   }
 
   quizReset()
 
+  quiz.find('#reset').click(function () {
+    quizReset()
+  })
+
   quiz.find('.answer').click(function () {
     $(this).parents('.question-wrap').hide()
+    quiz.find('#reset').html('Start Over')
 
     var dataType = $(this).attr('data-type')
     var dataVal = $(this).attr('data-val')
@@ -27,20 +34,22 @@ $(document).ready(function () {
   })
 
   var setResults = function () {
+    quiz.find('#reset').html('Retake the Quiz')
     var type = localStorage.getItem('type')
+    var days = localStorage.getItem('days')
+    var home = localStorage.getItem('home')
+    var numberGuests = localStorage.getItem('number-guests')
 
     if (type === 'host') {
-      var days = localStorage.getItem('days')
-      var home = localStorage.getItem('home')
-      var numberGuests = localStorage.getItem('number-guests')
-
       var results = quiz.find('#host-results')
       results.show()
 
       if (days === 'gt30') {
         results.find('.gt30-description').show()
+        results.find('.details').hide()
       } else {
         results.find('.gt30-description').hide()
+        results.find('.details').show()
         if (home === 'no' || numberGuests === 'gt2') {
           results.find('.headline .legality').addClass('illegal')
         }
@@ -52,9 +61,33 @@ $(document).ready(function () {
         }
       }
     }
-  }
 
-  quiz.find('#reset').click(function () {
-    quizReset()
-  })
+    if (type === 'traveler') {
+      var results = quiz.find('#traveler-results')
+      results.show()
+
+      if (days === 'gt30') {
+        results.find('.gt30-description').show()
+        results.find('.details').hide()
+      } else {
+        results.find('.gt30-description').hide()
+        results.find('.details').show()
+        if (home === 'no' || numberGuests === 'gt2') {
+          results.find('.headline .legality').addClass('illegal')
+        }
+        if (home === 'maybe' && numberGuests === 'lt3') {
+          results.find('.headline .legality').addClass('unknown')
+        }
+        if (home === 'no') {
+          results.find('li[result-type="home"]').addClass('illegal')
+        }
+        if (home === 'maybe') {
+          results.find('li[result-type="home"]').addClass('unknown')
+        }
+        if (numberGuests === 'gt2') {
+          results.find('li[result-type="number-guests"]').addClass('illegal')
+        }
+      }
+    }
+  }
 })
